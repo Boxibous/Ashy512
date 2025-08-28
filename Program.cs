572801bf45d8e986687bdbef95afa4da32acf410
ThunderStrike <noice22853@protonmail.com>
2025-08-28 07:49:01 -0500

    crazy add more

--- Program.cs
+++ Program.cs
@@ -44,31 +44,31 @@ class Program
         long strdep = 0;
         for (int i = 0; i < str.Length; i++)
         {
-            strdep += (long)(str[i] + (ulong)i ^ states[2]);
+            strdep += (long)(str[i] + ((ulong)i * states[(i + 2) % 8]) ^ states[i % 8]);
             states[i % 8] *= (~(str[i] * (ulong)0x483BC73FFF83FB << 0x4A) << (int)(strdep & 63)) ^ states[2];
-            states[i % 8] ^= Entropy(Entropy(states[(i + 1) % 8], (ulong)strdep, states[(i + 2) % 8] ^ states[i % 8]), states[(i + 5) % 8], Entropy((ulong)strdep, states[i % 8], (ulong)strdep));
+            states[i % 8] ^= Entropy(~Entropy(states[(i + 1) % 8], (ulong)strdep, states[(i + 2) % 8] ^ states[i % 8]), states[(i + 5) % 8], Entropy((ulong)strdep, states[i % 8], states[i % 8] ^ states[(i + 2) % 8]));
             states[(i + 1) % 8] *= ~(states[(i ^ 0x2) % 8] ^ states[i % 8]) ^ ~(ulong)strdep;
             states[(i + 2) % 8] ^= Entropy(((ulong)~str[i]), 1, states[i % 8]) ^ 0x27A48B;
             states[(i + 3) % 8] ^= (((ulong)str[i] << 0x1C | str[i]) ^ ((ulong)0x483BC73FFF83FB << 0x4A)) << (int)(strdep & 63);
             for (int k = 0; k < 8; k++)
             {
-                states[(i + 4) % 8] ^= (states[i % 8] ^ states[(i + 2) % 8]) & Entropy(states[(i + 3) % 8] ^ states[(i + 1) % 8], (ulong)(str[i] ^ k + 0xF5), (ulong)strdep);
-                states[(i + 5) % 8] ^= states[(i + 4) % 8] & states[(i + 2) % 8];
-                states[(i + 6) % 8] ^= Entropy(states[(i + 5) % 8], states[(i + 4) % 8], states[(i + 3) % 8]);
-                states[(i + 7) % 8] ^= Entropy(states[(i + 6) % 8], states[i % 8] ^ (ulong)k + 0xF5, states[(i % 8)]);
-                states[(i + k) % 8] ^= (states[(i + 7) % 8] & states[(i + 5) % 8]) | states[(i + 5) % 8] ^ Entropy(states[i % 8], (ulong)strdep, 0x483BC73FFF83FB);
+                states[(i + 4) % 8] ^= (states[i % 8] ^ states[(i + 2) % 8]) & Entropy(states[(i + 3) % 8] * ~states[(i + 1) % 8], (ulong)(str[i] ^ k + 0xF5), (ulong)strdep);
+                states[(i + 5) % 8] ^= ~states[(i + 4) % 8] & states[(i + 2) % 8];
+                states[(i + 6) % 8] ^= ~Entropy(states[(i + 5) % 8], states[(i + 4) % 8], states[(i + 3) % 8]);
+                states[(i + 7) % 8] ^= ~Entropy(states[(i + 6) % 8], states[i % 8] ^ (ulong)k + 0xF5, states[(i % 8)]);
+                states[(i + k) % 8] ^= (states[(i + 7) % 8] & states[(i + 5) % 8]) | states[(i + 5) % 8] ^ Entropy(states[i % 8], (ulong)strdep * ~states[k % 8], 0x483BC73FFF83FB);
             }
         }
-        for (int i = 0; i < 16; i++)
+        for (int i = 0; i < str.Length; i++)
         {
-            states[0] ^= Entropy(states[3] ^ states[2], states[1] & 63, (ulong)strdep);
-            states[1] ^= Entropy(states[0] ^ states[4], states[2] & 63, (ulong)strdep);
-            states[2] ^= Entropy(states[1] ^ states[5], states[3] & 63, (ulong)strdep);
-            states[3] ^= Entropy(states[2] ^ states[6], states[4] & 63, (ulong)strdep);
-            states[4] ^= Entropy(states[3] ^ states[7], states[5] & 63, (ulong)strdep);
-            states[5] ^= Entropy(states[4] ^ states[0], states[6] & 63, (ulong)strdep);
-            states[6] ^= Entropy(states[5] ^ states[1], states[7] & 63, (ulong)strdep);
-            states[7] ^= Entropy(states[6] ^ states[2], states[0] & 63, (ulong)strdep);
+            states[0] ^= Entropy(states[3] ^ states[2], (~states[1] * states[2]) & 63, (ulong)strdep ^ states[0]);
+            states[1] ^= Entropy(states[0] ^ states[4], (states[2] * states[1]) & 63, (ulong)strdep * ~states[1]);
+            states[2] ^= Entropy(states[1] ^ states[5], (states[3] ^ states[4]) & 63, (ulong)strdep ^ states[2]);
+            states[3] ^= Entropy(states[2] ^ states[6], (~states[4] ^ states[3]) & 63, (ulong)strdep * ~states[3]);
+            states[4] ^= Entropy(states[3] ^ states[7], (states[5] * states[6]) & 63, (ulong)strdep ^ states[4]);
+            states[5] ^= Entropy(states[4] ^ states[0], (states[6] ^ states[5]) & 63, (ulong)strdep ^ states[5]);
+            states[6] ^= Entropy(states[5] ^ states[1], (~states[7] * states[0]) & 63, (ulong)strdep * ~states[6]);
+            states[7] ^= Entropy(states[6] ^ states[2], (states[0] ^ states[1]) & 63, (ulong)strdep * ~states[7]);
         }
         byte[] bout = new byte[64];
         for (int i = 0; i < states.Length; i++)
